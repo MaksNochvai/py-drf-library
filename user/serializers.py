6f04f7217c3c4915d79980de0375663199e9dbe7
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-10 17:40:36 +0300

    Update user serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -3,11 +3,12 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=5, style={"input_type": "password"})
+
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "first_name", "last_name", "password", "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
