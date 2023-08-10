a82cef8f7c60eed542b4a6afec1b7f4a91638818
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-10 17:40:18 +0300

    Update user models.py

--- user/models.py
+++ user/models.py
@@ -43,11 +43,8 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
-    first_name = models.CharField(_("first name"), max_length=30, blank=True)
-    last_name = models.CharField(_("last name"), max_length=30, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
-
