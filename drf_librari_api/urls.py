54a3d7ad36fe72944ba4a35d30f2a936fad28f06
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-10 17:05:33 +0300

    Fixed & updated settings.py & urls.py

--- drf_librari_api/urls.py
+++ drf_librari_api/urls.py
@@ -12,6 +12,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("book.urls", namespace="books")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
