acb4d9505994867b88ea6ac1bc1a6e9a6cfd0273
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-10 15:50:19 +0300

    Added model Book in models.py

--- book/models.py
+++ book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+
+class CoverType(Enum):
+    HARD = 'Hardcover'
+    SOFT = 'Softcover'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=10, choices=[(cover.value, cover.name) for cover in CoverType])
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
