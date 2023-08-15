f6aeb61071a772473a8db8e437ad71a4e807ce51
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-16 00:31:14 +0300

    fix flake8

--- borrowings_service/urls.py
+++ borrowings_service/urls.py
@@ -7,7 +7,7 @@ app_name = "borrowings_service"
 urlpatterns = [
     path("", BorrowingViewSet.as_view({"get": "list", "post": "create"}), name="borrowing-list"),
     path("<pk>/", BorrowingViewSet.as_view(
-        {"get": "retrieve", "put": "update", "delete": "destroy"}),
+        {"get": "retrieve", "put": "update"}),
          name="borrowing-detail"
          ),
 ]
