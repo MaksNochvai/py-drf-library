df00097802b769406e35a876b378b911c3d629b5
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-15 17:32:49 +0300

    Added borrowings views.py

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowings_service.models import Borrowing
+from borrowings_service.serializers import BorrowingSerializer
+
+
+class BorrowingViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
