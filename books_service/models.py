532967cb3c285817a7e33fd0bd3842a31c47b0fe
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-22 12:08:15 +0300

    Did black for books_service

--- books_service/models.py
+++ books_service/models.py
@@ -3,14 +3,16 @@ from enum import Enum
 
 
 class CoverType(Enum):
-    HARD = 'Hardcover'
-    SOFT = 'Softcover'
+    HARD = "Hardcover"
+    SOFT = "Softcover"
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=10, choices=[(cover.value, cover.name) for cover in CoverType])
+    cover = models.CharField(
+        max_length=10, choices=[(cover.value, cover.name) for cover in CoverType]
+    )
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
 
